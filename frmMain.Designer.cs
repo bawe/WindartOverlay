f545a5967c05c743a20a16e68cf802c4eb31e413
bastian <bastian@weber-bastian.de>
2020-02-23 14:35:30 +0100

    Initial commit

---
+++ frmMain.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace WindartOverlay
+{
+    partial class frmMain 
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMain));
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.transparentControl1 = new TransparentControl();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.hideToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // imageList1
+            // 
+            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList1.Images.SetKeyName(0, "green_small.png");
+            this.imageList1.Images.SetKeyName(1, "dart48x48_3.png");
+            // 
+            // transparentControl1
+            // 
+            this.transparentControl1.BackColor = System.Drawing.SystemColors.Control;
+            this.transparentControl1.ContextMenuStrip = this.contextMenuStrip1;
+            this.transparentControl1.ForeColor = System.Drawing.Color.Transparent;
+            this.transparentControl1.Image = null;
+            this.transparentControl1.Location = new System.Drawing.Point(0, 0);
+            this.transparentControl1.Margin = new System.Windows.Forms.Padding(0);
+            this.transparentControl1.Name = "transparentControl1";
+            this.transparentControl1.Size = new System.Drawing.Size(48, 48);
+            this.transparentControl1.TabIndex = 1;
+            this.transparentControl1.Text = "transparentControl1";
+            this.transparentControl1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.transparentControl1_MouseDown);
+            this.transparentControl1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.transparentControl1_MouseMove);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.hideToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(100, 48);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // hideToolStripMenuItem
+            // 
+            this.hideToolStripMenuItem.Name = "hideToolStripMenuItem";
+            this.hideToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.hideToolStripMenuItem.Text = "Hide";
+            this.hideToolStripMenuItem.Click += new System.EventHandler(this.hideToolStripMenuItem_Click);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(48, 48);
+            this.ControlBox = false;
+            this.Controls.Add(this.transparentControl1);
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frmMain";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+            this.Text = "WindartOverlay";
+            this.TopMost = true;
+            this.TransparencyKey = System.Drawing.SystemColors.Control;
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.frmMain_FormClosing);
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.frmMain_FormClosed);
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            this.Shown += new System.EventHandler(this.frmMain_Shown);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private TransparentControl transparentControl1;
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem hideToolStripMenuItem;
+    }
+}
+
---
+++ frmMain.cs
@@ -0,0 +1,231 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Security.Principal;
+using System.Text;
+using System.Windows.Forms;
+using System.Xml.Serialization;
+
+
+using System.Threading.Tasks;
+using System.IO.Pipes;
+using System.IO;
+using System.Configuration;
+
+namespace WindartOverlay
+{
+    public partial class frmMain : Form
+    {
+        private const string PIPE_NAME = "DartOverlay";
+        private Point m_mouseposition;
+        private delegate void SafeCallDelegate(string text);
+
+        private bool m_hideatstart = false;
+
+        public frmMain()
+        {
+            InitializeComponent();
+
+            //this.FormBorderStyle = FormBorderStyle.FixedToolWindow;
+            this.ControlBox = false;
+            this.Text = String.Empty;
+            transparentControl1.Image = imageList1.Images[1];
+            this.FormBorderStyle = FormBorderStyle.None;
+
+            Util.DoUpgrade(Properties.Settings.Default);
+            
+            this.Location = Properties.Settings.Default.StartPos;
+            //if (m_hideatstart)
+            //    this.Hide();
+            
+            var server = Task.Factory.StartNew(() => RunServer(this));
+        }
+
+        public frmMain(string[] args) :this()
+        {
+            //if(args.Contains("/hide"))
+            //{
+            //    m_hideatstart = true;
+            //}
+
+            //if (args.Contains("/opacity"))
+            //{
+            //    Opacity = .4;
+            //}
+
+
+            for (int i = 0; i < args.Length; i++)
+            {
+                if(args[i] == "/hide")
+                    m_hideatstart = true;
+
+                if (args[i] == "/opacity")
+                {
+                    if(i++ < args.Length)
+                    {
+                        double opacity = double.Parse(args[i]);
+                        this.Opacity = opacity / 100;
+                    }
+                        
+                }
+            }
+
+            
+
+        }
+
+        static void RunServer(frmMain form)
+        {
+
+            // echo Hello > \\.\pipe\TestPipe1
+
+            while (true) { 
+                using (var pipeServer = new NamedPipeServerStream(PIPE_NAME, PipeDirection.In))
+                {
+                    pipeServer.WaitForConnection();
+                    using (var reader = new StreamReader(pipeServer))
+                    {
+                            string line = String.Empty;
+                            while ((line = reader.ReadLine()) != null)
+                            {
+                            Console.WriteLine(line);
+                            form.Event(line);
+                            }
+                        
+                    }
+                }
+            }
+        }
+
+
+
+        protected override CreateParams CreateParams
+        {
+            get
+            {
+                CreateParams cp = base.CreateParams;
+                // turn on WS_EX_TOOLWINDOW style bit
+                cp.ExStyle |= 0x80;
+                return cp;
+            }
+        }
+
+
+        public void Event(string message)
+        {
+            message = message.Trim();
+            //MessageBox.Show(message);
+
+
+            if (this.InvokeRequired)
+            {
+                var d = new SafeCallDelegate(Event);
+                this.Invoke(d, new object[] { message });
+            }
+            else
+            {
+                if (message == "hide")
+                    this.Visible = false;
+                if (message == "show")
+                    this.Visible = true;
+            }
+        }
+
+     
+
+        private void transparentControl1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                Point mousePos = Control.MousePosition;
+                mousePos.Offset(m_mouseposition.X, m_mouseposition.Y);
+                Location = mousePos;
+                Properties.Settings.Default.StartPos = this.Location;
+                ;
+            }
+        }
+
+        private void transparentControl1_MouseDown(object sender, MouseEventArgs e)
+        {
+            m_mouseposition = new Point(-e.X, -e.Y);
+        }
+
+
+
+        private void frmMain_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+ 
+        }
+
+        private void frmMain_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Properties.Settings.Default.StartPos = this.Location;
+            Properties.Settings.Default.Save();
+        }
+
+        public static class Util
+        {
+            // the name of the setting that flags whether we
+            // should perform an upgrade or not
+            private const string UpgradeFlag = "SettingsUpgrade";
+
+            public static void DoUpgrade(ApplicationSettingsBase settings)
+            {
+                try
+                {
+                    // attempt to read the upgrade flag
+                    if ((bool)settings[UpgradeFlag] == true)
+                    {
+                        // upgrade the settings to the latest version
+                        settings.Upgrade();
+
+                        // clear the upgrade flag
+                        settings[UpgradeFlag] = false;
+                        settings.Save();
+                    }
+                    else
+                    {
+                        // the settings are up to date
+                    }
+                }
+                catch (SettingsPropertyNotFoundException ex)
+                {
+                    // notify the developer that the upgrade
+                    // flag should be added to the settings file
+                    throw ex;
+                }
+            }
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void hideToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Visible = false;
+        }
+
+        private void frmMain_Shown(object sender, EventArgs e)
+        {
+            if (m_hideatstart)
+                this.Hide();
+
+            
+
+        }
+    }
+
+
+}
